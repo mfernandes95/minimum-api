ee0ddf92a04b65f93f8bb4c884fa298645c079f3
Matheus Fernandes Pinheiro <matheuspinheiro@sisconsultoria.com.br>
2023-08-05 01:31:37 -0300

    tests

---
+++ src/tests/__mocks__/typeorm.ts
@@ -0,0 +1,26 @@
+// export const createConnection = jest.fn();
+// export const getRepository = jest.fn().mockReturnValue({
+//   find: jest.fn().mockResolvedValue([
+//     {
+//         "id": 4,
+//         "name": "Karen Lee",
+//         "city": "Tokyo",
+//         "country": "Japan",
+//         "favorite_sport": "Swimming"
+//     },
+//     {
+//         "id": 5,
+//         "name": "Tom Brown",
+//         "city": "Sydney",
+//         "country": "Australia",
+//         "favorite_sport": "Running"
+//     },
+//     {
+//         "id": 6,
+//         "name": "Emma Wilson",
+//         "city": "Berlin",
+//         "country": "Germany",
+//         "favorite_sport": "Basketball"
+//     }
+//   ])
+// });
--- src/tests/user.spec.ts
+++ src/tests/user.spec.ts
@@ -1,12 +1,53 @@
-import supertest from 'supertest'
+import request from 'supertest'
 import app from '../app'
+import { getRepository } from 'typeorm';
 
-describe('User', () => {
-    test('should return users', async() => {
-        const result = await supertest(app).get('/api/users')
-        expect(result.statusCode).toEqual(200)
-    })
-})
+// describe('User', () => {
+//     test('should return users', async() => {
+//         const result = await request(app).get('/api/users')
+//         expect(result.statusCode).toEqual(200)
+//     })
+// })
+// jest.mock('typeorm', () => {
+    // repoMock.find.mockResolvedValue(['ok']);
+
+describe('GET /users', () => {
+
+//     beforeEach(() => {
+//     // Clear mock implementations and mock responses before each test
+//     jest.clearAllMocks();
+//   });
+
+    it('should return users', async () => {
+    // Mock the response from getRepository
+    // const mockUsers = [{ id: 1, name: 'John Doe', city: 'Tokyo', country: 'Australia',  favorite_sport: 'Running'}];
+    // (getRepository as jest.Mock).mockReturnValue({
+    //   find: jest.fn().mockResolvedValue(mockUsers),
+    // });
+        console.log("111111111111111111111111111111");
+    const response = await request(app).get('/api/users');
+
+    expect(response.status).toBe(200);
+    // expect(response.body).toEqual(mockUsers);
+  });
+
+    // it('should return users', async () => {
+    //   // Mock the response from getRepository
+    //   const mockUsers = [{ id: 1, name: 'John Doe', email: 'john@example.com' }];
+    //   (getRepository as jest.Mock).mockReturnValue({
+    //     find: jest.fn().mockResolvedValue(mockUsers),
+    //   });
+  
+    //   const response = await request(app).get('/api/users');
+  
+    //   expect(response.status).toBe(200);
+    //   expect(response.body).toEqual(mockUsers);
+    // });
+  
+    // Add other test cases as needed
+  });
+
+
 
 
 // import supertest from 'supertest'
